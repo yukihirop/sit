abafa60e8c9cf2e8fad31c310a0c21037de614c7
yukihirop <te108186@gmail.com>
2020-02-16 16:41:20 +0900

    Update clasp scripts (test)

--- test/localRepo/.sit/scripts/clasp/const.js
+++ test/localRepo/.sit/scripts/clasp/const.js
@@ -1,5 +1,7 @@
-var REMOTE_REF = 'refs/remotes';
-var REMOTE_LOG_REF = 'logs/refs/remotes'
-var MASTER_BRANCH = 'master';
-var REMOTE_REF_HEADER = ['branch', 'sha1'];
-var REMOTE_LOG_REF_HEADER = ['branch', 'beforesha', 'aftersha', 'username', 'email', 'unixtime', 'timezone', 'message'];
+const REMOTE_REF = 'refs/remotes';
+const REMOTE_LOG_REF = 'logs/refs/remotes'
+const MASTER_BRANCH = 'master';
+const REMOTE_REF_HEADER = ['branch', 'sha1'];
+const REMOTE_LOG_REF_HEADER = ['branch', 'beforesha', 'aftersha', 'username', 'email', 'unixtime', 'timezone', 'message'];
+const JA_DEFAULT_SHEET_NAME = 'シート1'
+const EN_DEFAULT_SHEET_NAME = 'Sheet1'
