db699fd44afd93bc25d5f7494cde1cd0371ff71e
yukihirop <te108186@gmail.com>
2020-03-08 15:00:32 +0900

    Add comment about fail test

--- src/main/repos/logs/__tests__/SitLogParser.spec.js
+++ src/main/repos/logs/__tests__/SitLogParser.spec.js
@@ -10,14 +10,45 @@ describe('SitLogParser', () => {
   describe('#parseToCSV', () => {
     describe('when logFile exist', () => {
       // FIXME:
+      // Why...?
+      /**
+       *   ● SitLogParser › #parseToCSV › when logFile exist › should return correctly
+
+          expect(received).toEqual(expected) // deep equality
+
+          - Expected  - 1
+          + Received  + 1
+
+          @@ -15,11 +15,11 @@
+                "8b58f3891ae3e4d274972a39d27fd460aaeaa6cc",
+                "yukihirop",
+                "<te108186@gmail.com>",
+                "1580961933681",
+                "+0900",
+          -     "clone: from 'https://docs.google.com/spreadsheets/d/1jihJ2crH31nrAxFVJtuC6fwlioCi1EbnzMwCDqqhJ7k/edit#gid=0'",
+          ",    "clone: from https://docs.google.com/spreadsheets/d/1jihJ2crH31nrAxFVJtuC6fwlioCi1EbnzMwCDqqhJ7k/edit#gid=0
+              ],
+              Array [
+                "master",
+                "8b58f3891ae3e4d274972a39d27fd460aaeaa6cc",
+                "2938ad2ab5722adf9b48ff5bac74989eaa2d144c",
+
+            16 |           .mockReturnValueOnce('8b58f3891ae3e4d274972a39d27fd460aaeaa6cc')
+            17 |
+          > 18 |         expect(model.parseToCSV()).toEqual([
+              |                                    ^
+            19 |           ["branch", "beforesha", "aftersha", "username", "email", "unixtime", "timezone", "message"],
+            20 |           ["master", "0000000000000000000000000000000000000000", "8b58f3891ae3e4d274972a39d27fd460aaeaa6cc", "yukihirop", "<te108186@gmail.com>", "1580961933681", "+0900", "clone: from 'https://docs.google.com/spreadsheets/d/1jihJ2crH31nrAxFVJtuC6fwlioCi1EbnzMwCDqqhJ7k/edit#gid=0'"],
+            21 |           ["master", "8b58f3891ae3e4d274972a39d27fd460aaeaa6cc", "2938ad2ab5722adf9b48ff5bac74989eaa2d144c", "yukihirop", "<te108186@gmail.com>", "1583639422044", "+0900", "commit Add good_bye"]
+       */
       xit('should return correctly', () => {
         model = new SitLogParser(repo, 'master', 'logs/refs/heads/master')
         jest.spyOn(repo, '_refBlobFromCommitHash').mockReturnValueOnce('0000000000000000000000000000000000000000')
-          .mockReturnValueOnce('4e2b7c47eb492ab07c5d176dccff3009c1ebc79b')
+          .mockReturnValueOnce('8b58f3891ae3e4d274972a39d27fd460aaeaa6cc')
 
         expect(model.parseToCSV()).toEqual([
           ["branch", "beforesha", "aftersha", "username", "email", "unixtime", "timezone", "message"],
-          ["master", "0000000000000000000000000000000000000000", "4e2b7c47eb492ab07c5d176dccff3009c1ebc79b", "yukihirop", "<te108186@gmail.com>", "1580961933681", "+0900", "clone: from https://docs.google.com/spreadsheets/d/1jihJ2crH31nrAxFVJtuC6fwlioCi1EbnzMwCDqqhJ7k/edit#gid=0"],
+          ["master", "0000000000000000000000000000000000000000", "8b58f3891ae3e4d274972a39d27fd460aaeaa6cc", "yukihirop", "<te108186@gmail.com>", "1580961933681", "+0900", "clone: from https://docs.google.com/spreadsheets/d/1jihJ2crH31nrAxFVJtuC6fwlioCi1EbnzMwCDqqhJ7k/edit#gid=0"],
           ["master", "8b58f3891ae3e4d274972a39d27fd460aaeaa6cc", "2938ad2ab5722adf9b48ff5bac74989eaa2d144c", "yukihirop", "<te108186@gmail.com>", "1583639422044", "+0900", "commit Add good_bye"]
         ])
       })
